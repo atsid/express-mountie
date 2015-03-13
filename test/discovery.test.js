d9d9dcd3def6899ae072798069e5fb59f54bd938
Chris Trevino <chris.trevino@atsid.com>
2015-03-13 10:56:01 -0700

    Adding a test for loading from a deeper path

--- test/discovery.test.js
+++ test/discovery.test.js
@@ -9,7 +9,7 @@ var chai = require('chai'),
 describe('Application Discovery', () => {
     it('finds one app in test apps folder', () => {
         let apps = mountie({
-            src: path.join(__dirname, '../test/data/test-apps'),
+            src: path.join(__dirname, 'data/test-apps'),
             parent: express()
         });
         expect(apps.length).to.equal(2);
@@ -18,7 +18,7 @@ describe('Application Discovery', () => {
     it('instantiates an express app ready to mount', () => {
         let app = express();
         let apps = mountie({
-            src: path.join(__dirname, '../test/data/test-apps'),
+            src: path.join(__dirname, 'data/test-apps'),
             parent: app
         });
         let subapp = apps[0];
@@ -33,14 +33,14 @@ describe('Application Discovery', () => {
     });
 
     it('fails if no express app is provided', () => {
-        let config = { src: path.join(__dirname, '../test/data/test-apps') };
+        let config = { src: path.join(__dirname, 'data/test-apps') };
         expect(() => mountie(config)).to.throw();
     });
 
     it('can select the mount path using a prefix string', () => {
         let app = express();
         let apps = mountie({
-            src: path.join(__dirname, '../test/data/test-apps'),
+            src: path.join(__dirname, 'data/test-apps'),
             parent: app,
             prefix: "/api"
         });
@@ -51,7 +51,18 @@ describe('Application Discovery', () => {
     it ('can select the mount path using a prefix function', () => {
         let app = express();
         let apps = mountie({
-            src: path.join(__dirname, '../test/data/test-apps'),
+            src: path.join(__dirname, 'data/test-apps'),
+            parent: app,
+            prefix: appName => "/api/" + appName
+        });
+        expect(apps[0].mountpath).to.equal('/api/test1');
+        expect(apps[1].mountpath).to.equal('/api/test2');
+    });
+
+    it ('can select applications in a deep path', () => {
+        let app = express();
+        let apps = mountie({
+            src: path.join(__dirname, 'data/test-deep-path/test-apps'),
             parent: app,
             prefix: appName => "/api/" + appName
         });
@@ -62,7 +73,7 @@ describe('Application Discovery', () => {
     it ('can mount nested applications', () => {
         let app = express();
         let apps = mountie({
-            src: path.join(__dirname, '../test/data/nested-apps'),
+            src: path.join(__dirname, 'data/nested-apps'),
             parent: app,
             prefix: appName => "/api/" + appName
         });
